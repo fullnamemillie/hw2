1539b0ab84658086a15d2570731767c3bf904eef
__ <__>
2025-07-27 16:12:50 +0300

    hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -15,12 +15,18 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector((state: AppStoreType) => state.loading.isLoading);
 
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
+    const dispatch = useDispatch();
 
-        // setTimeout
+    // const isLoading = false
+
+    const setLoading = () => {
+        dispatch(loadingAC(true));
+
+        setTimeout(() => {
+            dispatch(loadingAC(false));
+        }, 1500)
     }
 
     return (
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,17 +2,24 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): StateType => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
+
+        case "CHANGE_LOADING": {
+            return {...state, isLoading: action.isLoading}
+        }
 
         default:
-            return state
+            return state;
     }
 }
 
 type LoadingActionType = {
     type: 'CHANGE_LOADING'
+    isLoading: boolean
+}
+
+type StateType = {
     isLoading: boolean
 }
 
