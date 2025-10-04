fe0aa79ffefdb26bea1994ee5099e42b3e130e5e
Alexey Ryashin <fullnamemillie@gmail.com>
2025-10-04 22:29:11 +0300

    hw15

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -1,54 +1,55 @@
-import React from 'react'
+import React from "react";
 
-// добавить в проект иконки и импортировать
-const downIcon = '[\\/]'
-const upIcon = '[/\\]'
-const noneIcon = '[--]'
+const downIcon = "▼";
+const upIcon = "▲";
+const noneIcon = "⇅";
 
 export type SuperSortPropsType = {
-    id?: string
-    sort: string
-    value: string
-    onChange: (newSort: string) => void
-}
+  id?: string;
+  sort: string; // текущее состояние сортировки (например: '1tech', '0tech', '')
+  value: string; // имя колонки для сортировки (например: 'tech' или 'developer')
+  onChange: (newSort: string) => void;
+};
 
-export const pureChange = (sort: string, down: string, up: string) => {
-    // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-    return up // исправить
-}
+/**
+ * pureChange – переключает состояние сортировки:
+ * '' -> down -> up -> ''
+ */
+export const pureChange = (sort: string, down: string, up: string): string => {
+  // если сейчас сортировка вниз для этой колонки – переключаем на вверх
+  if (sort === down) return up;
+  // если сейчас вверх – сбрасываем сортировку
+  if (sort === up) return "";
+  // если сортировка выключена или стоит на другой колонке – включаем вниз
+  return down;
+};
 
-const SuperSort: React.FC<SuperSortPropsType> = (
-    {
-        sort, value, onChange, id = 'hw15',
-    }
-) => {
-    const up = '0' + value
-    const down = '1' + value
+const SuperSort: React.FC<SuperSortPropsType> = ({
+  sort,
+  value,
+  onChange,
+  id = "hw15",
+}) => {
+  // '1' + value = сортировка по возрастанию
+  const up = "0" + value;
+  // '0' + value = сортировка по убыванию
+  const down = "1" + value;
 
-    const onChangeCallback = () => {
-        onChange(pureChange(sort, down, up))
-    }
+  const onChangeCallback = () => {
+    onChange(pureChange(sort, down, up));
+  };
 
-    const icon = sort === down
-        ? downIcon
-        : sort === up
-            ? upIcon
-            : noneIcon
+  const icon = sort === down ? downIcon : sort === up ? upIcon : noneIcon;
 
-    return (
-        <span
-            id={id + '-sort-' + value}
-            onClick={onChangeCallback}
-        >
-            {/*сделать иконку*/}
-            {/*<img*/}
-            {/*    id={id + '-icon-' + sort}*/}
-            {/*    src={icon}*/}
-            {/*/>*/}
+  return (
+    <span
+      id={id + "-sort-" + value}
+      onClick={onChangeCallback}
+      style={{ cursor: "pointer", userSelect: "none" }}
+    >
+      {icon}
+    </span>
+  );
+};
 
-            {icon} {/*а это убрать*/}
-        </span>
-    )
-}
-
-export default SuperSort
+export default SuperSort;
